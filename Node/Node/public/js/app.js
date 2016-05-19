59f2abd8b53f98db5c488ba08db62bfe869c0b4f
Dale Chen <daleche@microsoft.com>
2016-05-19 16:44:56 +0800

    Upload the sample

---
+++ Node/Node/public/js/app.js
@@ -0,0 +1,130 @@
+﻿    var app = angular.module("app", ['ngRoute']);
+    
+    app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+            
+            $routeProvider
+            .when('/userList', {
+                templateUrl: "templates/userList.html", 
+                controller: 'UserController'
+            })
+            .when('/addUser', {
+                templateUrl: "templates/addUser.html", 
+                controller: 'addUserController'
+            })
+            .when('/wishrequest', {
+                templateUrl: "templates/wishrequest.html", 
+                controller: 'WishRequestController'
+            })
+            .otherwise({ redirectTo: '/' });
+            $locationProvider.hashPrefix('!').html5Mode(true);
+        }]);
+    
+    var api = {
+        connect: "/api/connect",
+        disconnect: "/api/disconnect",
+        checkConnection: "/api/checkConnection",
+        user: "/api/user/",
+    };
+    app.controller('IndexController', function ($rootScope, $scope, $http) {
+        $rootScope.isConnect = false;
+        $scope.checkConnection = function () {
+            $http.get(api.checkConnection)
+                .then(function (response) {
+                $rootScope.isConnect = response.data;
+            });
+        };
+        $scope.checkConnection();
+        $scope.connect = function () {
+            console.log('connect');
+            $http.get(api.connect)
+                .success(function (response) {
+                $rootScope.isConnect = true;
+            });
+        };
+        $scope.disconnect = function () {
+            console.log('disconnect');
+            $http.get(api.disconnect)
+                .success(function (response) {
+                $rootScope.isConnect = false;
+            });
+        };
+    });
+    
+app.controller('UserController', function ($rootScope, $scope, $http, $filter) {
+        $scope.users = [];
+        $scope.form = {};
+        $scope.isEdit = false;
+        $http.get(api.user)
+            .success(function (response) {
+                console.log(response)
+                $scope.users = response;
+            });
+        $scope.del = function (id) {
+            $http.delete(api.user + id).then(function (response) {
+                if (response.status === 200) {
+                    $scope.users = $filter('filter')($scope.users, { id: '!' + id })
+                    $rootScope.msgalert = 'success';
+                    $rootScope.msg = 'Delete user successfully!';
+                }
+            }, function (err) {
+                console.log(err)
+                $rootScope.msgalert = 'danger';
+                $rootScope.msg = err.statusText + ": " + err.data;
+            });
+        };
+         $scope.edit = function (index) {
+            $scope.isEdit = true;
+            $scope.editIndex = index;
+            $scope.form = {
+                displayName: $scope.users[index].displayName,
+                userPrincipalName: $scope.users[index].userPrincipalName
+            };
+        };
+        $scope.submit = function (){
+            $http.patch(api.user + $scope.users[$scope.editIndex].id, $scope.form)
+                .then(function (response) {
+                    if (response.status === 200) {
+                        $rootScope.msgalert = 'success';
+                        $rootScope.msg = 'Update user successfully!';
+                        $http.get(api.user + $scope.users[$scope.editIndex].id).then(function (data) {
+                            console.log(data.data)
+                            $scope.users[$scope.editIndex] = data.data;
+                            $scope.cancel();
+                        })
+                    }
+                }, function (err) {
+                    console.log(err)
+                    $rootScope.msgalert = 'danger';
+                    $rootScope.msg = err.statusText + ": " + err.data;
+                });
+        }
+        $scope.cancel = function () {
+            $scope.form = {};
+            $scope.isEdit = false;
+            delete $scope.editIndex;
+        };
+    });
+    
+    app.controller('addUserController', function ($rootScope, $scope, $http, $location) {
+        $scope.form = {
+            accountenabled:"true",
+            forceChangePasswordNextSignIn:"true"
+        };
+        $scope.submit = function () {
+            console.log($scope.form)
+            $http.post(api.user, angular.copy($scope.form))
+            .then(function (response) {
+                if (response.status === 200) {
+                    $rootScope.msgalert = 'success';
+                    $rootScope.msg = 'Create user successfully!';
+                    $location.path('/userList')
+                } 
+            }, function (err) {
+                console.log(err)
+                $rootScope.msgalert = 'danger';
+                $rootScope.msg = err.statusText+": "+err.data;
+            });
+        };
+
+    });
+
---
+++ Node/Node/routes/api.js
@@ -0,0 +1,140 @@
+﻿var express = require('express');
+var router = express.Router();
+var auth = require('../helpers/auth.js');
+var config = require('../helpers/config.js');
+var request = require('request');
+/* GET users listing. */
+router.get('/connect', function (req, res) {
+    auth.getAccessToken().then(function (data){
+        console.log(data);
+        req.session.access_token = data;
+        res.send('respond with a resource');
+    }).catch(function (error) {
+        console.log('fail', error);
+        res.send(500,error)
+    });
+});
+
+router.get('/disconnect', function (req, res) {
+    if (delete req.session.access_token) {
+        console.log(req.session)
+        res.send(true)
+    } else {
+        res.send(500,false)
+    }
+});
+
+router.get('/checkConnection', function (req, res) {
+    if (req.session.access_token!==undefined) {
+        res.send(true)
+    } else {
+        res.send(false)
+    }   
+});
+router.get('/user/(:user_id)?', function (req, res) {
+    var endpoint = 'https://graph.microsoft.com/v1.0/users' + (req.params.user_id?'/' + req.params.user_id:'');
+    console.log(endpoint)
+    request.get(endpoint+"?"+config.filder, {
+        'auth': {
+            'bearer': req.session.access_token
+        }
+    }, function (err, response, body) {
+        var parsedBody = JSON.parse(body);
+        console.log(parsedBody);
+        if (err) {
+            res.send(500,err);
+        } else if (parsedBody.error) {
+            res.send(500,parsedBody.error.message);
+        } else {
+            // The value of the body will be an array of all users.
+            if (req.params.user_id) {
+                res.send(parsedBody);
+            } else {
+                res.send(parsedBody.value);
+            }
+        }
+    });
+});
+
+router.post('/user', function (req, res) {
+    var endpoint = 'https://graph.microsoft.com/v1.0/users';
+    var data = req.body;
+    data.passwordProfile = {
+        "forceChangePasswordNextSignIn": data.forceChangePasswordNextSignIn=="true"?true:false,
+        "password": data.password
+    }
+    data.accountenabled=data.accountenabled=="true"?true:false;
+    delete data.password;
+    delete data.forceChangePasswordNextSignIn;
+    console.log(data);
+    request.post({
+        url:endpoint,
+        body: JSON.stringify(data),
+        headers:{'Content-type': 'application/json'},
+        'auth': {
+            'bearer': req.session.access_token
+        }
+    }, function (err, response, body) {
+        var parsedBody = JSON.parse(body);
+        
+        if (err) {
+            console.log(err);
+            res.send(500, err);
+        } else if (parsedBody.error) {
+            console.log(parsedBody.error);
+            res.send(500, parsedBody.error.message);
+        } else {
+            // The value of the body will be an array of all users.
+            res.send(parsedBody.value);
+        }
+    });
+});
+
+router.patch('/user/:user_id', function (req, res) {
+    var endpoint = 'https://graph.microsoft.com/v1.0/users/'+req.params.user_id;
+    var data = req.body;
+    console.log(data);
+    console.log(endpoint);
+    request.patch({
+        url: endpoint,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json',
+            'authorization':'Bearer '+ req.session.access_token
+        }
+    }, function (err, response, body) {
+        //var parsedBody = JSON.parse(body);
+        console.log(response.statusCode)
+        
+        if (err) {
+            console.log(err);
+            res.send(500, err);
+        //} else if (parsedBody.error) {
+        //    console.log(parsedBody.error);
+        //    res.send(500, parsedBody.error.message);
+        } else {
+            // The value of the body will be an array of all users.
+            res.send(body);
+        }
+    });
+});
+
+router.delete('/user/:user_id', function (req, res) {
+    var endpoint = 'https://graph.microsoft.com/v1.0/users/' + req.params.user_id;
+    request.del({
+        url: endpoint,
+        headers: {
+            'authorization': 'Bearer ' + req.session.access_token
+        }
+    }, function (err, response, body) {
+        console.log(response.statusCode)
+        if (err) {
+            console.log(err);
+            res.send(500, err);
+        } else {
+            res.send(body);
+        }
+    });
+});
+
+module.exports = router;
